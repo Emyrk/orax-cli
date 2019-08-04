4479052269045e49530c78ac0ab5404fd2cd36e7
Paul Bernier <bernier.pja@gmail.com>
2019-08-04 10:15:02 -0700

    feat: randomly delay submission within time window

--- orax/client.go
+++ orax/client.go
@@ -1,7 +1,9 @@
 package orax
 
 import (
+	"math/rand"
 	"sync"
+	"time"
 
 	"github.com/sirupsen/logrus"
 
@@ -90,35 +92,44 @@ func (cli *Client) listenSignals() {
 			}
 			cli.miner.Mine(v.OprHash, cli.wscli.NoncePrefix, int(v.MaxNonces))
 		case *msg.SubmitSignalMessage:
-			if cli.miner.IsRunning() {
-				ms := cli.miner.Stop()
-
-				msm := new(msg.MinerSubmissionMessage)
-				msm.OprHash = ms.OprHash
-
-				msm.Nonces = make([]msg.Nonce, len(ms.OrderedBestNonces))
-				for i, nonce := range ms.OrderedBestNonces {
-					msm.Nonces[i] = msg.Nonce{Nonce: nonce.Nonce, Difficulty: nonce.Difficulty}
-				}
-				msm.HashRate = uint64(float64(ms.TotalOps) / ms.Duration.Seconds())
-
-				log.WithFields(logrus.Fields{
-					"nonces":   msm.Nonces,
-					"oprHash":  msm.OprHash,
-					"hashRate": msm.HashRate,
-				}).Infof("Submitting mining result")
-
-				data, err := msm.Marshal()
-				if err != nil {
-					log.Error("Failed to marshal MinerSubmissionMessage: ", err)
-				} else {
-					cli.wscli.Send(data)
-				}
-			} else {
-				log.Info("Skipping submission as no mining session was running.")
-			}
+			cli.submitMiningResult(time.Duration(v.WindowDurationSec) * time.Second)
 		default:
 			log.Warnf("Unexpected message %T!\n", v)
 		}
+	}
+}
+
+func (cli *Client) submitMiningResult(windowDuration time.Duration) {
+	if cli.miner.IsRunning() {
+		ms := cli.miner.Stop()
+
+		msm := new(msg.MinerSubmissionMessage)
+		msm.OprHash = ms.OprHash
+
+		msm.Nonces = make([]msg.Nonce, len(ms.OrderedBestNonces))
+		for i, nonce := range ms.OrderedBestNonces {
+			msm.Nonces[i] = msg.Nonce{Nonce: nonce.Nonce, Difficulty: nonce.Difficulty}
+		}
+		msm.HashRate = uint64(float64(ms.TotalOps) / ms.Duration.Seconds())
+
+		log.WithFields(logrus.Fields{
+			"nonces":   msm.Nonces,
+			"oprHash":  msm.OprHash,
+			"hashRate": msm.HashRate,
+		}).Infof("Submitting mining result")
+
+		data, err := msm.Marshal()
+		if err != nil {
+			log.Error("Failed to marshal MinerSubmissionMessage: ", err)
+		} else {
+			// Randomly delay the reply within acceptable time window
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			jitter := time.Duration(int64(float64(windowDuration.Nanoseconds()) / 2 * r.Float64()))
+			timer := time.NewTimer(jitter)
+			<-timer.C
+			cli.wscli.Send(data)
+		}
+	} else {
+		log.Info("Skipping submission as no mining session was running.")
 	}
 }
